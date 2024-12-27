0bb7ffda1899da45ee35b5210f4814af60dd4ceb
Frederico Villar <fredvillar64@gmail.com>
2024-12-27 15:16:54 -0500

    - Removed ANIMAL_WEBURL and database imports from webscraping_animal

--- passgen_webscraper/webscraper/get_data/webscraping_animal.py
+++ passgen_webscraper/webscraper/get_data/webscraping_animal.py
@@ -1,13 +1,8 @@
-from api.api_models.animal import AnimalModel
-from db.table_manage.animal_table import insert_one
 from selenium.webdriver.common.by import By
 import pandas
 from logging_config import logger
 
-ANIMAL_WEBURL = "https://a-z-animals.com/animals/"
-
 async def get_animal_data(driver):
-    driver.get(ANIMAL_WEBURL)
     div_results = driver.find_elements(By.XPATH,"//li")
     animal_list = []
     for div_result in div_results:
